3cdf1e2cdc87df36fcd726ff917226a7e5cf5732
pranavavadivel <pranavavadivel01@gmail.com>
2024-03-01 18:09:26 +0530

    deleting my old bootstrap path in src

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect } from "react";
 import { Form, Button, Input, Typography } from "antd";
-// import { FallOutlined } from "@ant-design/icons";
 
 function App() {
   async function fecthing(movieTitle) {
